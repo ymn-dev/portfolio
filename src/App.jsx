12c14f50389863b6afaa57d236819ecce85c6e5c
ymn-dev <sornniyom.a@gmail.com>
2023-09-20 10:37:26 +0700

    init daisyui tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Navbar from "./Components/Navbar/Navbar";
+import Footer from "./Components/Footer/Footer";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Navbar />
+      <div>App</div>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
